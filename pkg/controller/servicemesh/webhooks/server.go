15c5116c1f6c8c2b348804b60378d8e8cb036150
Jonh Wendell <jonh.wendell@redhat.com>
2021-06-08 17:15:32 -0400

    MAISTRA-2404: Register SME conversion webhook only when CRD is present (#733)
    Because SME is a control plane CRD, it's only installed after the first
    reconcile. We should make sure the conversion webhook is registered only
    when the CRD is present, which could be on initialization, or after the
    first reconcile.

--- pkg/controller/servicemesh/webhooks/server.go
+++ pkg/controller/servicemesh/webhooks/server.go
@@ -23,7 +23,7 @@ var (
 	smcpValidatorServicePath = "/validate-smcp"
 	smcpMutatorServicePath   = "/mutate-smcp"
 	smcpConverterServicePath = "/convert-smcp"
-	smeConverterServicePath  = "/convert-sme"
+	SmeConverterServicePath  = "/convert-sme"
 	smmrValidatorServicePath = "/validate-smmr"
 	smmrMutatorServicePath   = "/mutate-smmr"
 	smmValidatorServicePath  = "/validate-smm"
@@ -34,13 +34,8 @@ func Add(mgr manager.Manager) error {
 	ctx := common.NewContextWithLog(common.NewContext(), log)
 	log.Info("Configuring Maistra webhooks")
 
-	if common.Config.OLM.WebhookManagementDisabled == true {
-		log.Info("Webhook Config Management is disabled via olm configuration")
-	} else {
-		operatorNamespace := common.GetOperatorNamespace()
-		if err := createWebhookResources(ctx, mgr, log, operatorNamespace); err != nil {
-			return err
-		}
+	if err := createWebhookResources(ctx, mgr, log, common.GetOperatorNamespace()); err != nil {
+		return err
 	}
 
 	watchNamespaceStr, err := k8sutil.GetWatchNamespace()
@@ -55,7 +50,7 @@ func Add(mgr manager.Manager) error {
 	hookServer.Register(smcpConverterServicePath, &conversion.Webhook{})
 
 	log.Info("Adding Maistra ServiceMeshExtension conversion handler")
-	hookServer.Register(smeConverterServicePath, &conversion.Webhook{})
+	hookServer.Register(SmeConverterServicePath, &conversion.Webhook{})
 
 	log.Info("Adding Maistra ServiceMeshControlPlane validation handler")
 	hookServer.Register(smcpValidatorServicePath, &webhook.Admission{
