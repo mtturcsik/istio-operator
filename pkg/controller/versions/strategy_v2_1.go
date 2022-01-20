e080ade816fae909e516a5c25a9555776d6c2803
Mate Turcsik <mate.turcsik1@ibm.com>
2022-01-20 16:11:46 +0000

    removed unused code and fixed chart enablements

--- pkg/controller/versions/strategy_v2_1.go
+++ pkg/controller/versions/strategy_v2_1.go
@@ -211,48 +211,6 @@ func (v *versionStrategyV2_1) GetChartInstallOrder() [][]string {
 	return v2_1ChartOrder
 }
 
-func (v *versionStrategyV2_1) isExternalProfileActive(profiles []string) bool {
-	for _, profile := range profiles {
-		if profile == "external" {
-			return true
-		}
-	}
-	return false
-}
-
-// func (v *versionStrategyV2_1) checkAndSetupRemoteDataPlaneConfig(istio *v1.HelmValues, log logr.Logger) {
-// 	if isComponentEnabled(istio, v2_1ChartMapping[RemoteChart].enabledField) {
-// 		log.Info("Remote chart used. Disabling everything else.")
-// 		discoveryChartDetails := v2_1ChartMapping[DiscoveryChart]
-// 		discoveryChartDetails.enabledField = "noway"
-// 		v2_1ChartMapping[DiscoveryChart] = discoveryChartDetails
-// 		meshConfigChartDetails := v2_1ChartMapping[MeshConfigChart]
-// 		meshConfigChartDetails.enabledField = "noway"
-// 		v2_1ChartMapping[MeshConfigChart] = meshConfigChartDetails
-// 		telemetryCommonChartDetails := v2_1ChartMapping[TelemetryCommonChart]
-// 		telemetryCommonChartDetails.enabledField = "noway"
-// 		v2_1ChartMapping[TelemetryCommonChart] = telemetryCommonChartDetails
-// 	}
-// }
-//
-// func (v *versionStrategyV2_1) checkAndSetupExternalControlPlaneConfig(externalProfileFound bool, log logr.Logger) {
-// 	if externalProfileFound {
-// 		log.Info("External Control Plane profile used. Disabling everything else, except the discovery Chart.")
-// 		meshConfigChartDetails := v2_1ChartMapping[MeshConfigChart]
-// 		meshConfigChartDetails.enabledField = "noway"
-// 		v2_1ChartMapping[MeshConfigChart] = meshConfigChartDetails
-// 		telemetryCommonChartDetails := v2_1ChartMapping[TelemetryCommonChart]
-// 		telemetryCommonChartDetails.enabledField = "noway"
-// 		v2_1ChartMapping[TelemetryCommonChart] = telemetryCommonChartDetails
-// 		gatewayEgressChartDetails := v2_1ChartMapping[GatewayEgressChart]
-// 		gatewayEgressChartDetails.enabledField = "noway"
-// 		v2_1ChartMapping[GatewayEgressChart] = gatewayEgressChartDetails
-// 		gatewayInressChartDetails := v2_1ChartMapping[GatewayIngressChart]
-// 		gatewayInressChartDetails.enabledField = "noway"
-// 		v2_1ChartMapping[GatewayIngressChart] = gatewayInressChartDetails
-// 	}
-// }
-
 // TODO: consider consolidating this with 2.0 rendering logic
 func (v *versionStrategyV2_1) Render(ctx context.Context, cr *common.ControllerResources, cniConfig cni.Config, smcp *v2.ServiceMeshControlPlane) (map[string][]manifest.Manifest, error) {
 	log := common.LogFromContext(ctx)
@@ -288,11 +246,16 @@ func (v *versionStrategyV2_1) Render(ctx context.Context, cr *common.ControllerR
 		return nil, err
 	}
 
-	externalProfileFound := v.isExternalProfileActive(spec.Profiles)
-
 	// In case of split control and data plane, the CNI installation is disabled for the control plane side.
 	// However, this field controls the injector for the data plane, which uses CNI (and so not using real init container, only validator)
-	err = spec.Istio.SetField("istio_cni.enabled", cniConfig.Enabled || externalProfileFound)
+	var isCNIEnabledInSMCP bool
+	if smcp.Spec.CNI != nil {
+		isCNIEnabledInSMCP = *smcp.Spec.CNI.Enabled
+	} else {
+		isCNIEnabledInSMCP = false
+	}
+
+	err = spec.Istio.SetField("istio_cni.enabled", cniConfig.Enabled || isCNIEnabledInSMCP)
 	if err != nil {
 		return nil, fmt.Errorf("Could not set field status.lastAppliedConfiguration.istio.istio_cni.enabled: %v", err)
 	}
@@ -302,21 +265,27 @@ func (v *versionStrategyV2_1) Render(ctx context.Context, cr *common.ControllerR
 	}
 
 	if smcp.Spec.ControlPlaneMode != nil {
-		err = spec.Istio.SetField("istioDiscovery.enabled", smcp.Spec.ControlPlaneMode.ControlPlaneModeEnabled)
+		err = spec.Istio.SetField("istioDiscovery.enabled", *smcp.Spec.ControlPlaneMode.Enabled)
 	} else {
 		err = spec.Istio.SetField("istioDiscovery.enabled", true)
 	}
 
 	if smcp.Spec.Telemetry != nil {
-		err = spec.Istio.SetField("telemetry.common.enabled", smcp.Spec.Telemetry.TelemetryEnabled)
+		err = spec.Istio.SetField("telemetry.common.enabled", *smcp.Spec.Telemetry.Enabled)
 	} else {
 		err = spec.Istio.SetField("telemetry.common.enabled", true)
 	}
 
 	if smcp.Spec.RemoteMode != nil {
-		err = spec.Istio.SetField("remote.enabled", smcp.Spec.RemoteMode.RemoteModeEnabled)
+		err = spec.Istio.SetField("remote.enabled", *smcp.Spec.RemoteMode.Enabled)
 	} else {
 		err = spec.Istio.SetField("remote.enabled", false)
+	}
+
+	if smcp.Spec.Gateways != nil {
+		err = spec.Istio.SetField("gateways.enabled", *smcp.Spec.Gateways.Enabled)
+	} else {
+		err = spec.Istio.SetField("gateways.enabled", true)
 	}
 
 	// Override these globals to match the install namespace
@@ -421,12 +390,6 @@ func (v *versionStrategyV2_1) Render(ctx context.Context, cr *common.ControllerR
 		}
 	}
 
-	// Remote Data Plane check and setup if so
-	// v.checkAndSetupRemoteDataPlaneConfig(spec.Istio, log)
-
-	// External Control Plane check and setup if so
-	// v.checkAndSetupExternalControlPlaneConfig(externalProfileFound, log)
-
 	// convert back to the v2 type
 	smcp.Status.AppliedSpec = v2.ControlPlaneSpec{}
 	err = cr.Scheme.Convert(&smcp.Status.AppliedValues, &smcp.Status.AppliedSpec, nil)
@@ -479,7 +442,7 @@ func (v *versionStrategyV2_1) Render(ctx context.Context, cr *common.ControllerR
 		}
 	}
 
-	if isComponentEnabled(spec.Istio, "gateways") && !externalProfileFound {
+	if isComponentEnabled(spec.Istio, "gateways") {
 		log.V(2).Info("rendering gateways charts")
 		if origGateways, ok := values.AsMap()["gateways"]; ok {
 			if origGatewaysMap, ok := origGateways.(map[string]interface{}); ok {
